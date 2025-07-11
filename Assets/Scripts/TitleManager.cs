d04d4922d6168106574dcb45b9c22f72dea882e7
JoelAguilellaMolina <al415406@uji.es>
2025-07-11 13:07:38 +0200

    1.0 con créditos
    Añadida la version acreditada

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -10,6 +10,7 @@ public class TitleManager : MonoBehaviour
     public Button Jugar;
     public Button Créditos;
     public DataSave DataSave;
+    public Button Salir;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -24,6 +25,17 @@ public class TitleManager : MonoBehaviour
             SceneManager.LoadScene("Mapa");
         }
         else if(Jugar.enabled == false && !DataSave.nivelesCompletados[0]) SceneManager.LoadScene("Nivel1");
+        if(Salir.enabled == false) 
+        {
+        #if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+        }
+
         //else if(Jugar.enabled == false && !DataSave.nivelesCompletados[0]) 
     }
 }
